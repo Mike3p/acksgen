f6503114ae03f8f85f81bad9480f931eba9c9825
Mike3P <m.hoh18@gmail.com>
2021-06-14 08:50:48 +0200

    first venture into javascript.

--- Tools/charedit/character_editor_page.py
+++ Tools/charedit/character_editor_page.py
@@ -5,6 +5,7 @@ from flask import Flask, flash, request, redirect, url_for
 from Tools.chargen.characterGenerator import load_character, get_yaml_of_character
 from Tools.chargen.character import Character
 import yaml
+from flask_jsonpify import jsonify
 from werkzeug.utils import secure_filename
 
 bp = Blueprint('character_editor_page', __name__, url_prefix='/charedit')
@@ -23,9 +24,26 @@ def upload_character():
         character_object = load_character(yaml.safe_load(character_file))
         session['uploaded_character'] = character_object
 
-        return render_template('pages/charedit.html', char = session.get('uploaded_character').__repr__())
+        #return render_template('pages/charedit.html', char = session.get('uploaded_character').__repr__())
+        return jsonify(result=session.get('uploaded_character').__repr__())
     else:
-        return render_template('pages/charedit.html', char = ["no file chosen!"])
+        #return render_template('pages/charedit.html', char = ["no file chosen!"])
+        return jsonify(result="No File chosen!")
+
+# @bp.route('/levelup', methods=('GET', 'POST'))
+# def levelup_character():
+#     if session.get('uploaded_character'):
+#         try:
+#             c: Character = session.get('uploaded_character')
+#             c.levelup(c.level+1,25)
+#             session['uploaded_character'] = c
+#         except:
+#             return """
+#             <h1 style='color: red;'>Invalid Character File!</h1>
+#             """
+#         #return render_template('pages/charedit.html', char = session.get('uploaded_character').__repr__())
+#         return render_template('pages/charedit.html', embed=get_yaml_of_character(session.get('uploaded_character')))
+#     return render_template('pages/charedit.html', char = ["no character uploaded!"])
 
 @bp.route('/levelup', methods=('GET', 'POST'))
 def levelup_character():
@@ -38,7 +56,8 @@ def levelup_character():
             return """
             <h1 style='color: red;'>Invalid Character File!</h1>
             """
-        return render_template('pages/charedit.html', char = session.get('uploaded_character').__repr__())
+        return jsonify(char=c.__repr__())
+        #return render_template('pages/charedit.html')
     return render_template('pages/charedit.html', char = ["no character uploaded!"])
 
 @bp.route('/download', methods = ('GET', 'POST'))
